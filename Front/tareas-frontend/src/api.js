cfa98a0f2b5de523c3765dd9c936023bd52ba00f
LautaroMendezPortela <lautaromendezportela@gmail.com>
2026-05-04 23:34:27 -0300

    cambio a modo produccion

--- Front/tareas-frontend/src/api.js
+++ Front/tareas-frontend/src/api.js
@@ -1,2 +1,2 @@
-//export const API_URL = "https://tareas-api-gymi.onrender.com";
-export const API_URL = "http://localhost:3000";
+export const API_URL = "https://tareas-api-gymi.onrender.com";
+// export const API_URL = "http://localhost:3000";
